bbafffc4b0154c699f01b3e527b50933e85e1546
rafaelparayno <35307253+rafaelparayno@users.noreply.github.com>
2020-07-09 05:09:18 +0800

    Update context.php

--- system/pages/paypal/context.php
+++ system/pages/paypal/context.php
@@ -12,8 +12,8 @@ use PayPal\Auth\OAuthTokenCredential;
 use PayPal\Rest\ApiContext;
 
 
-$clientId = 'Ae1CzR_e7WWZhF1-VGr4tnNp4fiPN7PolsNDLtxXWvTAvUC7DUEAox6YQ20X7O0D9049dga5N2xZ3qFV';
-$clientSecret = 'ELgksb_UShYf1Ydm5SZXcINb0FT5kPXRcUuw9ef5vqw4Co0wiCznhLbxEnG90yIYgxmDUk6Ce0e9pE0P';
+$clientId = 'SECRET';
+$clientSecret = 'SECRET';
 
 $apiContext = new ApiContext(
     new OAuthTokenCredential(
